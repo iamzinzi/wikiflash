16e1e631477b8aa1e39b7b843c3c7c016d1b41ee
Heindrick Cheung <hcheung01@gmail.com>
2019-02-15 20:41:50 +0000

    latest template

--- jsfolder/first.js
+++ jsfolder/first.js
@@ -1,25 +1,44 @@
-$(document).ready(function(){
-  $("#submit_it").click(function(){
-    $("#container_id").hide();
-  });
-});
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/version1.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <title>WikiFlash</title>
+  </head>
+  <body>
+    <div id='container_id' class="container">
+      <div class="logo_box">
+        <img id='flash_logo' class='logo' src="images/manta-ray-black-silhouette.jpg" alt="logo">
+      </div>
+      <div class="word_input">
+        <form action="/action_page.py">
+          <input type="text" id="fname" name="firstname" placeholder="Your start word..">
+          <input type="text" id="lname" name="lastname" placeholder="Your end word..">
+          <input id='submit_it' type="button" value="Submit">
+        </form>
+      </div>
+    </div>
+  </div>
 
+  <div class="display_wrapper">
+    <div class="status_box_id" id='status_box_id'>
+        <h3 id='demo'></h3>
+        <div class="">
 
-$(document).ready(function(){
-  $("#submit_it").click(function(){
-    $(".display_wrapper").show();
-  });
-});
+        </div>
+        <div class="">
 
-$(document).ready(function(){
-  $("#return_button").click(function(){
-    $(".display_wrapper").hide();
-  });
-});
+        </div>
+        <div class="">
 
-$(document).ready(function(){
-  $("#return_button").click(function(){
-    $("#container_id").show();
-  });
-});
- 
+        </div>
+        <div class="">
+
+        </div>
+        <button id="return_button" type="button" name="button" value="Refresh Page" onClick="refresh">Return</button>
+    </div>
+  </div>
+    <script src="jsfolder\first.js"></script>
+  </body>
+</html>
