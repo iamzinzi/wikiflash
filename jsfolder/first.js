1ae2b8c2f4fd82bc7d78ea132360a86f298929c5
Heindrick Cheung <hcheung01@gmail.com>
2019-02-15 16:58:31 +0000

    mockup first version merge request if possible

---
+++ jsfolder/first.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $("#submit_it").click(function(){
+    $("#container_id").hide();
+  });
+});
+
+
+$(document).ready(function(){
+  $("#submit_it").click(function(){
+    $(".display_wrapper").show();
+  });
+});
+
+$(document).ready(function(){
+  $("#return_button").click(function(){
+    $(".display_wrapper").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#return_button").click(function(){
+    $("#container_id").show();
+  });
+});
+ 
