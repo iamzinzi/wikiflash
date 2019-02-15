de2d7fde7735bea90282b1be61c24bbcd30809f7
JackWanaCode <kazucuong@yahoo.com>
2019-02-15 21:37:09 +0000

    update with request

--- web_flask/wiki_route.py
+++ web_flask/wiki_route.py
@@ -1,31 +1,19 @@
 """
 starts a Flash web app
 """
-from flask import flask, render_template, jsonify
+from flask import flask, render_template, request
 app = Flask(__name__)
-import function
+from get_path import get_path
 app.url_map.strict_slashes = False
 
 
-
-@app.errorhandler(404)
-def not_found(error):
-    """Gives error message when any invalid
-    url are requested
-    """
-    return jsonify({"error": "Not found"}), 404
-
-
-@app_views.route('/s_word/<s_word>/e_word/<e_word>', methods=["GET"])
-def return_cities(state_id):
+@app.route('/web_flask/s_word/<s_word>/e_word/<e_word>', methods=["GET"])
+def result(state_id):
     """return the function"""
-    dic = {"s_word": sword, "e_word": e_word}
-    lis = function(dic)
-    if lis is None:
-        abort(404)
-    else:
-        return render_template('index.html', lis)
+    result = request.form
+    lis = get_path(s_word, e_word)
+    return render_template('index.html', lis)
 
 
 if __name__ == '__main__':
-    app.run(host="35.235.70.198", port=4000)
+    app.run(host="35.235.70.198", port=5000)
